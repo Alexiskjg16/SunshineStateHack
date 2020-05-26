01e386e8b7503cb24aa7e7b1a47634450de8e7bc
Alexis K Grisham <alexisgrisham@AlexisGMac.lan1>
2020-05-26 14:44:44 -0400

    fixed bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TeamComponent } from './team/team.component';
@@ -17,8 +16,9 @@ const routes: Routes = [
   { path: 'news-component', component: NewsComponent },
   { path: 'home-component', component: HomeComponent },
   { path: 'donate-component', component: DonateComponent },
+  { path: '',   redirectTo: 'home-component', pathMatch: 'full' },
 ]
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
